28bb8770660b689a78a4236423df5a2aed5e6b74
AlbertStephenGerenaCastellanos <algecass94@gmail.com>
2021-07-12 21:09:38 -0500

    Operadores de asignación 12-Julio-2021

---
+++ main.js
@@ -0,0 +1,66 @@
+let a = 27;
+let c = 8;
+let d = 4;
+
+//Operador de asignación
+console.log(`Operador de asignación\n`);
+console.log(`Valor de a = ${a}`);
+
+
+// Operador de Adicion
+console.log(`\nOperador de adición \n`);
+console.log(`Valor c ${c}`);
+console.log(`Valor d ${d}`);
+c += d;
+//c = c + d
+console.log(`Para el operador de adición el nuevo valor de c es ${c} y su tipo de datos es`, typeof(c));
+
+// Operador de Sustracion
+c = 8;
+d = 4;
+c -= d;
+// c = c - d;
+console.log(`\nOperador de sustracción\n`);
+console.log(`Para el operador de sustracción el nuevo valor de c es ${c} y su tipo de datos es`, typeof(c));
+
+// Operador de Multiplicacion
+c = 8;
+d = 4;
+c *= d;
+//c = c * d;
+console.log(`\nOperador de multiplicación\n`);
+console.log(`Para el operador de multiplicación el nuevo valor de c es ${c} y su tipo de datos es`, typeof(c));
+
+// Operador de División
+c = 8;
+d = 4;
+c /= d;
+//c = c / d;
+console.log(`\nOperador de división\n`);
+console.log(`Para el operador de división el nuevo valor de c es ${c} y su tipo de datos es`, typeof(c));
+
+// Operador Resto
+console.log(`\nOperador de resto\n`);
+c = 7;
+d = 3;
+console.log(`Valor c ${c}`);
+console.log(`Valor d ${d}`);
+c %= d;
+//c = c % d;
+console.log(`Para el operador de resto el resultado es ${c} y su tipo de datos es`, typeof(c));
+
+
+// Operador exponenciación
+c = 8;
+d = 4;
+c **= d;
+//c = c elevado d;
+console.log(`\nOperador de exponencación\n`);
+console.log(`Para el operador de exponenciación el resultado es es ${c} y su tipo de datos es`, typeof(c));   
+
+
+
+
+
+
+
